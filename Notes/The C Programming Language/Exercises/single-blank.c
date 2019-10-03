565e773fdae36139eeb57ef8b515d8bb9851739d
Katie Carrel <katiecarrel@gmail.com>
2019-10-03 10:12:21 -0700

    added exercise for single blanks

---
+++ Notes/The C Programming Language/Exercises/single-blank.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#define NONBLANK '-'
+
+main() {
+    int c,lastc;
+    lastc=NONBLANK;
+
+    while((c=getchar())!=EOF)
+    {
+        if(c==' ')
+        {
+            if(lastc!=' ')
+                putchar(c);
+        }
+        else
+            putchar(c);
+        lastc=c;
+    }
+}

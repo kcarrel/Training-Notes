41afacb437e761d7ab8b8c3ddf016bfea28e3a41
Katie Carrel <katiecarrel@gmail.com>
2019-10-25 14:45:40 -0700

    made adjustments to the base strrindex code

--- Notes/The C Programming Language/Exercises/strrindex.c
+++ Notes/The C Programming Language/Exercises/strrindex.c
@@ -3,45 +3,43 @@
 #define MAXLINE 1000
 
 int getinput(char line[], int max);
-int strindex(char source[], char searchfor[]);
+int strrindex(char s[], char t[]);
 
-char pattern[] = "ould";
 
 /* find all lines matching pattern*/
 int main(void)
 {
-    char line[MAXLINE];
+    char s[] = "abcdedfabcde";
+    char t[] = "abc";
     int found = 0;
-    while (getinput(line, MAXLINE) > 0)
-        if (strindex(line, pattern) >= 0) {
-            printf("%s", line);
-            found++;
-        }
-    return found;
-}
+    // while (getinput(s, MAXLINE) > 0)
+    found  = strrindex(s, t);
+    printf("Result: %d\n", found);
+    }
 
-/* getinput: get line into s, return length*/
-int getinput(char s[], int lim)
-{
-    int c,i;
-    i = 0;
-    while (--lim > 0 && (c=getchar()) != EOF && c != '\n')
-        s[i++] = c;
-    if (c == '\n')
-        s[i++] = c;
-    s[i] = '\0';
-    return i;
-}
+// /* getinput: get line into s, return length*/
+// int getinput(char s[], int lim)
+// {
+//     int c,i;
+//     i = 0;
+//     while (--lim > 0 && (c=getchar()) != EOF && c != '\n')
+//         s[i++] = c;
+//     if (c == '\n')
+//         s[i++] = c;
+//     s[i] = '\0';
+//     return i;
+// }
 
 /* strindex: return index of t in s, -1 if none*/
-int strindex(char s[], char t[])
+int strrindex(char s[], char t[])
 {
-    int i, j, k;
+    int i, j, k,result;
+    result = -1;
     for (i = 0; s[i] != '\0'; i++) {
         for (j=i, k=0; t[k] != '\0' && s[j]==t[k]; j++, k++)
         ;
         if (k > 0 && t[k] == '\0')
-            return i;
+            result = i;
     }
-    return -1;
+    return result;
 }

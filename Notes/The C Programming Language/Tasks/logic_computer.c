a6e28aa140e5f985c02fc3d281c6a24164cd224a
Katie Carrel <katiecarrel@gmail.com>
2019-11-04 16:36:14 -0800

    added naive solution for comparing ands

--- Notes/The C Programming Language/Tasks/logic_computer.c
+++ Notes/The C Programming Language/Tasks/logic_computer.c
@@ -3,6 +3,9 @@
 #define INPUT_SIZE 100
 
 int getinput(char string[], int max);
+int compare_or(char arg1[], char arg2[]);
+int compare_and(char arg1[], char arg2[]);
+
 
 int main(void) 
 {
@@ -12,11 +15,11 @@ int main(void)
     sscanf(string1,"%s %s %s %s", logic, arg1, command, arg2);
     if ( strcmp(command, "or") == 0) 
         {
-            printf("POC or");
+            compare_or(arg1, arg2);
         } 
         else if ( strcmp(command, "and") == 0)
         {
-            printf("POC and");
+            compare_and(arg1, arg2);
         }
         
         else
@@ -38,3 +41,28 @@ int getinput(char string[], int max)
     string[i]='\0';
 }
 
+int compare_or(char arg1[], char arg2[])
+{
+     if ( strcmp(arg1, "true") == 0);
+}
+
+int compare_and(char arg1[], char arg2[])
+{
+     if ( strcmp(arg1, "true") == 0) 
+     {
+         if ( strcmp(arg2, "true") == 0) 
+         {
+             printf("true");
+         } else {
+         printf("false");
+        }
+     } else if ( strcmp(arg1, "false") == 0) 
+     {
+        if ( strcmp(arg2, "false") == 0) 
+        {
+            printf("true");
+        } else {
+         printf("false");
+        }
+     }
+}

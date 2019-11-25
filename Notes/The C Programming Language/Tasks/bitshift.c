286308664a852c80faeedf45f5ee4602b2225c9a
Katie Carrel <katiecarrel@gmail.com>
2019-11-25 11:17:32 -0800

    testing

--- Notes/The C Programming Language/Tasks/bitshift.c
+++ Notes/The C Programming Language/Tasks/bitshift.c
@@ -5,29 +5,6 @@
 
 #define INPUT_SIZE 100
 
-int getinput(char string[], int max);
-int shift(int bits, char direction, int value);
-
-
-/* Take in user input
-format to bitshift, bits passed, the direction to shift and how many 
-*/
-int main(void)
-{
-
-    char string1[INPUT_SIZE], bitshift[9], bits[50], direction[8], num[INPUT_SIZE];
-    int value, bitarray[50];
-    getinput(string1, INPUT_SIZE);
-    sscanf(string1,"%s %s %s %s", bitshift, bits, direction, num);
-    value = atoi(num);
-    /* This bit(heheh) is not working */
-    bitarray = atoi(bits);
-    if ( strcmp(bitshift, "bitshift") == 0) 
-    {
-        printf("%u", shift(bitarray, direction, value));
-    }
-}
-
 int getinput(char string[], int max)
 {
     int i, c;
@@ -41,13 +18,32 @@ int getinput(char string[], int max)
     string[i]='\0';
 }
 
-int shift(int bits, char direction, int value)
+int shift(int *bits, char *direction, int *value)
 {
-    if (strcmp(direction, "right") == 0)
+    printf("%d \n", *value);
+    printf("%d \n", *bits);
+
+    // if (strcmp(direction, "right") == 0)
+    // {
+    //     return bits >> value;
+    // } else if (strcmp(direction, "left") == 0)
+    // {
+    //     return bits << value;
+    // }
+}
+
+/* Take in user input
+format to bitshift, bits passed, the direction to shift and how many 
+*/
+int main(void)
+{
+
+    char string1[INPUT_SIZE], bitshift[INPUT_SIZE], direction[INPUT_SIZE];
+    int bits[INPUT_SIZE], num[INPUT_SIZE];
+    getinput(string1, INPUT_SIZE);
+    sscanf(string1,"%s %d %s %d", bitshift, bits, direction, num);
+    if ( strcmp(bitshift, "bitshift") == 0) 
     {
-        return bits >> value;
-    } else if (strcmp(direction, "left") == 0)
-    {
-        return bits << value;
+        shift(bits, direction, num);
     }
 }

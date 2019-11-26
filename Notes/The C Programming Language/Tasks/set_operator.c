229bbc5865e8baf6725a7e78e6d55b438191774f
Katie Carrel <katiecarrel@gmail.com>
2019-11-26 14:35:59 -0800

    added a hacky workaround for regex limitations in c so as to interpret the arrays being passed to program by user for comparison

--- Notes/The C Programming Language/Tasks/set_operator.c
+++ Notes/The C Programming Language/Tasks/set_operator.c
@@ -19,18 +19,28 @@ int getinput(char string[], int max)
 }
 
 
-void product(int num1, int num2, int num3) 
-{
-   printf("Do that product thing.");
+void product(char *num1, char *num2, char *num3) 
+{   
+    printf("Num1: %s \n", num1);
+    printf("Num2: %s \n", num2);
+    printf("Num3: %s \n", num3);
+
+    printf("Do that product thing.");
 }
 
-void uni(int num1, int num2, int num3) 
+void uni(char *num1, char *num2, char *num3) 
 {
-   printf("%s", "Do that union thing.");
+    printf("Num1: %s \n", num1);
+    printf("Num2: %s \n", num2);
+    printf("Num3: %s \n", num3);
+    printf("%s", "Do that union thing.");
 }
 
-void intersection(int num1, int num2, int num3) {
-   printf("%s", "Do that product thing.");
+void intersection(char *num1, char *num2, char *num3) {
+    printf("Num1: %s \n", num1);
+    printf("Num2: %s \n", num2);
+    printf("Num3: %s \n", num3);
+    printf("%s", "Do that product thing.");
 }
 
 
@@ -40,11 +50,10 @@ Used function name uni instead of union due to reserved word issue
 int main(void)
 {
 
-    char string1[INPUT_SIZE], compute[INPUT_SIZE], command[INPUT_SIZE];
+    char string1[INPUT_SIZE], compute[INPUT_SIZE], command[INPUT_SIZE], array1[INPUT_SIZE], array2[INPUT_SIZE], array3[INPUT_SIZE];
     int bits[INPUT_SIZE], num[INPUT_SIZE];
-    int array1 = 0, array2 = 0, array3 = 0;
     getinput(string1, INPUT_SIZE);
-    sscanf(string1,"%s %s", compute, command);
+    sscanf(string1,"%s %s %*c%[^\"]%*c %*c%[^\"]%*c %*c%[^\"]", compute, command, array1, array2, array3);
     if ( strcmp(compute, "compute") == 0) {
         if ( strcmp(command, "product") == 0) {
             product(array1, array2, array3);
@@ -58,5 +67,6 @@ int main(void)
     } 
     else {
         printf("Program cannot understand input. Please use correct form.\n");
+ 
     }
 }

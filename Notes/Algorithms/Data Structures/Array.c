932fdb4a9b42c1068a30312e55eefb4532881a60
Katie Carrel <katiecarrel@gmail.com>
2020-01-06 11:02:42 -0800

    added a function that allows the insertion of a provided value in the array at a certain index

--- Notes/Algorithms/Data Structures/Array.c
+++ Notes/Algorithms/Data Structures/Array.c
@@ -1,14 +1,59 @@
 #include <stdio.h>
 #define MAX_ARRAY 10
 
-void main() {
+void traverse(int *arr) {
+    int i;
+    for (i = 0; i < MAX_ARRAY; i++) {
+        printf("%d \n", arr[i]);
+    }
+}
+void insert(int *arr, int index, int value) {
+    int shifted, i;
+    shifted = MAX_ARRAY + 1;
+    for (i = shifted; i >= index; i--) {
+        arr[i] = arr[i - 1];
+    }
+    arr[index - 1] = value;
+    
+    for (i = 0; i < shifted; i++) {
+        printf("%d \n", arr[i]);
+    };
+    
+}
+
+void delete(int *arr, int index) {
+
+}
+
+void search(int *arr, int target) {
+
+}
+
+void update(int *arr, int index, int value) {
+
+}
+
+void test_insert() {
+
+}
+
+void test_delete() {
+
+}
+
+void test_search() {
+
+}
+void test_update() {
+
+}
+
+int main(void) {
     int ARRAY[MAX_ARRAY], i, val = 0;
     for (i = 0; i < MAX_ARRAY; i++) {
         ARRAY[i] = val;
         val = val + 10;
     }
-
-    for (i = 0; i < MAX_ARRAY; i++) {
-        printf("%d \n", ARRAY[i]);
-    }
+    insert(ARRAY, 2, 77);
+    
 }

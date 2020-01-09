3b305fd896eb64a3cca4b63e818f80236fc833e8
Katie Carrel <katiecarrel@gmail.com>
2020-01-09 14:39:46 -0800

    moved functions/testing functions/structures around to be grouped together

--- Notes/Algorithms/Data Structures/Array.c
+++ Notes/Algorithms/Data Structures/Array.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 #define MAX_ARRAY 10
 #define SUCCESS 0
 #define ERROR -1
 #define OVERFLOW -2
+
 
 void traverse(int *arr) {
     int i;
@@ -21,6 +23,24 @@ void traverse(int *arr) {
     //     };
 }
 
+//Create uses malloc to dynamically allocate memory for a specific size
+//Returns -1 if allocation fails and pointer to array if allocation succeeds 
+int * create() {
+    int* ptr;
+    int i, val = 0;
+    ptr = (int*)malloc(MAX_ARRAY * sizeof(int));
+
+    // if (ptr == NULL) {
+    //     return -1;
+    // } else {
+        for (i = 0; i < MAX_ARRAY; i++) {
+            ptr[i] = val;
+            val = val + 10;
+        }
+        return ptr;
+    
+}
+
 //Insert: Inserts a value at the provided index in the given array
 // Returns a 0 if successful, -1 if an error occurs and -2 if overflow occurs
 int insert(int *arr, int index, int value) {
@@ -32,45 +52,6 @@ int insert(int *arr, int index, int value) {
             arr[i] = arr[i - 1];
         }
         arr[index - 1] = value;
-        return SUCCESS;
-    } 
-    return ERROR;
-}
-
-//Delete: Deletes in-place the value at the provided index in the given array
-//Returns a 0 if successfull, -1 if an error occurs and -2 if overflow occurs
-int delete(int *arr, int index) {
-    int j = index + 1, i;
-    int o = sizeof(arr);
-    if (index <= o && index >= 0) {
-        for (i = index; i < MAX_ARRAY; i++) {
-            arr[i] = arr[j];
-            j++;
-        }
-        return SUCCESS;
-    } else {
-        return ERROR;
-    }    
-}
-
-//Search: Traverses a given array and checks if the target value is present
-//Returns a 0 if target is found, -1 if the search is unsuccessful 
-int search(int *arr, int target) {
-    int i;
-    for (i = 0; i < MAX_ARRAY; i++) {
-        if (arr[i] == target) {
-            return SUCCESS;
-        }
-    }
-    return ERROR;
-}
-
-//Update: Updates the element of an array at a given index to equal the provided value
-//Returns a 0 if update is successful or -1 if an error occurs
-int update(int *arr, int index, int value) {
-    int o = sizeof(arr);
-    if (index <= o |&& index >= 0) {
-        arr[index] = value;
         return SUCCESS;
     } 
     return ERROR;
@@ -111,6 +92,22 @@ void test_insert(int *arr) {
     }
 }
 
+//Delete: Deletes in-place the value at the provided index in the given array
+//Returns a 0 if successfull, -1 if an error occurs and -2 if overflow occurs
+int delete(int *arr, int index) {
+    int j = index + 1, i;
+    int o = sizeof(arr);
+    if (index <= o && index >= 0) {
+        for (i = index; i < MAX_ARRAY; i++) {
+            arr[i] = arr[j];
+            j++;
+        }
+        return SUCCESS;
+    } else {
+        return ERROR;
+    }    
+}
+
 struct test_delete {
     int index;
 };
@@ -136,6 +133,19 @@ void test_delete(int *arr) {
     for (i = 0; i < 10; i++) {
         delete(arr, delete_index[i].index);
     }
+}
+
+
+//Search: Traverses a given array and checks if the target value is present
+//Returns a 0 if target is found, -1 if the search is unsuccessful 
+int search(int *arr, int target) {
+    int i;
+    for (i = 0; i < MAX_ARRAY; i++) {
+        if (arr[i] == target) {
+            return SUCCESS;
+        }
+    }
+    return ERROR;
 }
 
 struct test_search {
@@ -165,6 +175,16 @@ void test_search(int *arr) {
     }
 }
 
+//Update: Updates the element of an array at a given index to equal the provided value
+//Returns a 0 if update is successful or -1 if an error occurs
+int * update(int *arr, int index, int value) {
+    int o = sizeof(arr);
+    if (index <= o && index >= 0) {
+        arr[index] = value;
+        return arr;
+    } 
+}
+
 struct test_update {
     int index;
     int value;
@@ -185,10 +205,13 @@ struct test_update update_value[10] = {
 
 //Takes in an array and runs the array with testing update_values through update function then uses compare function
 //Returns 0 if arrays match and -1 if arrays do not match expectations
-void test_update(int *arr) {
+void test_update() {
+
     int i;
+    int *p;
+    p = create();
     for (i = 0; i < 10; i++) {
-        update(arr, update_value[i].index, update_value[i].value);
+        update(p, update_value[i].index, update_value[i].value);
     }
 }
 
@@ -205,13 +228,8 @@ int compare(int *arr1, int *arr2) {
     return SUCCESS;
 }
 
-//To-Do: Main creates a dynamic array, loops 10 times to add in element values increasing in 10 each loop
-//Calls table testing functions 
+
+//Main calls table testing functions 
 int main(void) {
-    int ARRAY[MAX_ARRAY], i, val = 0;
-    for (i = 0; i < 10; i++) {
-        ARRAY[i] = val;
-        val = val + 10;
-    }
-    test_insert(ARRAY);
+    test_update();
 }

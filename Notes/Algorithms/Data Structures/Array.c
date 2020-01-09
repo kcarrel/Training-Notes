7c99ff7a4a3062954513f2e7c285e9acc7ee3e75
Katie Carrel <katiecarrel@gmail.com>
2020-01-09 11:19:21 -0800

    Added in comments for each array function that describe what they do and the return values expected. Added in a struct for table values for implementing table testing with test_insert.

--- Notes/Algorithms/Data Structures/Array.c
+++ Notes/Algorithms/Data Structures/Array.c
@@ -1,31 +1,40 @@
 #include <stdio.h>
 #define MAX_ARRAY 10
+#define SUCCESS 0
+#define ERROR -1
+#define OVERFLOW -2
 
 void traverse(int *arr) {
     int i;
     for (i = 0; i < MAX_ARRAY; i++) {
         printf("%d \n", arr[i]);
     }
+    //insert traverse
+    // for (i = 0; i < shifted; i++) {
+    //     printf("%d \n", arr[i]);
+    // };
+
+    //delete traverse
+    // int k = sizeof(arr);
+    //     for (i = 0; i <= k; i++) {
+    //         printf("Index %d: %d \n", i, arr[i]);
+    //     };
 }
 
-//To-do: in lieu of dynamically allocating space we could just create a new array with a size equal to shifted
-void insert(int *arr, int index, int value) {
+//Insert: Inserts a value at the provided index in the given array
+// Returns a 0 if successful, -1 if an error occurs and -2 if overflow occurs
+int insert(int *arr, int index, int value) {
     int shifted, i;
     shifted = MAX_ARRAY + 1;
-    int result[shifted];
     for (i = shifted; i >= index; i--) {
         arr[i] = arr[i - 1];
     }
     arr[index - 1] = value;
-
-    for (i = 0; i < shifted; i++) {
-        printf("%d \n", arr[i]);
-    };
-    
 }
 
-// To-do: Not sure if there is a way to calculate the changed array size while still deleting "in place" just yet
-void delete(int *arr, int index) {
+//Delete: Deletes in-place the value at the provided index in the given array
+//Returns a 0 if successfull, -1 if an error occurs and -2 if overflow occurs
+int delete(int *arr, int index) {
     int j = index + 1, i;
     int o = sizeof(arr);
     if (index <= o) {
@@ -33,74 +42,75 @@ void delete(int *arr, int index) {
             arr[i] = arr[j];
             j++;
         }
-        int k = sizeof(arr);
-        for (i = 0; i <= k; i++) {
-            printf("Index %d: %d \n", i, arr[i]);
-        };
+        return SUCCESS;
     } else {
-        printf("The index you have specified is outside of the provide array's range!");
-    }
-
-    
+        return ERROR;
+    }    
 }
 
+//Search: Traverses a given array and checks if the target value is present
+//Returns a 0 if target is found, -1 if the search is unsuccessful 
 int search(int *arr, int target) {
     int i;
     for (i = 0; i < MAX_ARRAY; i++) {
         if (arr[i] == target) {
-            printf("You target value %d has been found at index %d. \n", target, i);
-            return 0;
+            return SUCCESS;
         }
     }
-    printf("Target not found. \n");
-    return 0;
+    return ERROR;
 }
 
-void update(int *arr, int index, int value) {
-    arr[index] = value;
-    traverse(arr);
+//Update: Updates the element of an array at a given index to equal the provided value
+//Returns a 0 if update is successful or -1 if an error occurs
+int update(int *arr, int index, int value) {
+    int o = sizeof(arr);
+    if (index <= o) {
+        arr[index] = value;
+        return SUCCESS;
+    } 
+    return ERROR;
 }
 
 // Todo: touch base with Sasha re Go table tests --> C table tests
-// 
+//
+
+struct test_insert {
+    int index;
+    int value;
+};
+struct test_insert insert_values[10] = {
+    {11, 2},
+    {5, 2000},
+    {5, 1000},
+    {29, 50000},
+    {11, 28},
+    {-2, 82},
+    {0, 87},
+    {1, 222},
+    {-6, 2},
+    {10, 290}
+};
 void test_insert(int *arr) {
-    printf("Round 1: \n");
-    insert(arr, 11, 2);
-
-    printf("Round 2: \n");
-    insert(arr, 5, 2000);
-
-    printf("Round 3: \n");
-    insert(arr, 5, 1000);
+    int i;
+    for (i = 0; i < 10; i++) {
+        insert(arr, insert_values[i].index, insert_values[i].value);
+    }
 }
 
 void test_delete(int *arr) {
-    printf("Round 1: \n");
     delete(arr, 1);
-    printf("Round 2: \n");
     delete(arr, 1);
-    printf("Round 3: \n"); 
     delete(arr, 11);
 }
 
 void test_search(int *arr) {
-    printf("Round 1: \n");
     search(arr, 100);
-
-    printf("Round 2: \n");
     search(arr, -100);
-
-    printf("Round 3: \n"); 
     search(arr, 20);
 }
 void test_update(int *arr) {
-    printf("Round 1: \n");
     update(arr, 9, 100);
-
-    printf("Round 2: \n");
     update(arr, 0, 19);
-
-    printf("Round 3: \n"); 
     update(arr, 200, 9);
 
 }
@@ -109,20 +119,18 @@ int compare(int *arr1, int *arr2) {
     int i;
     for (i = 1; i <= MAX_ARRAY; i++) {
         if (arr1[i] != arr2[i]) { 
-            return 0;
+            return SUCCESS;
         }
      }
-    return 1;
+    return ERROR;
 }
 
+//To-Do: Main creates a dynamic array, loops 10 times to add in element values increasing in 10 each loop
+//Calls table testing functions 
 int main(void) {
     int ARRAY[MAX_ARRAY], i, val = 0;
     for (i = 0; i < 10; i++) {
         ARRAY[i] = val;
         val = val + 10;
     }
-    // test_delete(ARRAY);
-    // test_insert(ARRAY);
-    // test_search(ARRAY);
-    // test_update(ARRAY);
 }

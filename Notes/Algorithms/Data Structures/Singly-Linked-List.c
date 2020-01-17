3ea4969c7b8db997b578d5eda58aeab4bcea5b2e
Katie Carrel <katiecarrel@gmail.com>
2020-01-16 17:01:14 -0800

    ignoring stuff

--- Notes/Algorithms/Data Structures/Singly-Linked-List.c
+++ Notes/Algorithms/Data Structures/Singly-Linked-List.c
@@ -3,20 +3,25 @@
 // use macros to define alloc failure and return 
 #define ALLOC_NODE 
 
+#define SUCCESS 0
+#define ERROR -1
+
 //node struct
 struct node {
     int value;
     struct node * next;
 };
 
-//createNode creates a node 
+
+
+//createNode creates a node and makes it the next node to the provided head array
 //Returns the new node if the memory allocation is successful
 //Prints an error message if memory allocation fails 
-struct node * createNode(int val, struct node * next) {
+struct node * createNextNode(int val) {
     struct node * temp = malloc(sizeof(*temp));
     if (temp != NULL) {
         temp->value = val;
-        temp->next = next;
+        temp->next = NULL;
         return temp;
     } else {
         printf("Memory Allocation failure.");
@@ -35,15 +40,39 @@ SinglyLinkedList * createLinkedList() {
     return temp;
 }
 
+//appendNode adds a node to the end of linked list
+//Returns 0 if add was successful, -1 if failure occurs
+int appendNode(struct node* temp,struct node* last) {
+    return SUCCESS;
+}
 
+//deleteNode
+
+//Traverse LinkedList
 
 // repetition of a complex allocation procedure:
 
 //     checking for allocation errors
 //     the whole thing can be a function
 
+void buildList(){
+    int result;
+    struct node* head = (struct node*) malloc(sizeof(struct node)); 
+    head->value = 0; 
+    struct node* temp = head;  
+    int i, testVals[10] = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+    for (i = 0; i <= 10; i++) {
+        struct node * last = createNextNode(testVals[i]);
+        result = appendNode(temp, last);
+        if (result == 0) {
+            temp = last;
+        }
+    }
+}
 
+//Main first builds the testing singly linked list by calling the helper function buildList
 void main() {
+   buildList();
 
 
 }

2e9d20425d63b24f0fc0312cd2e7cf04a015ff94
Katie Carrel <katiecarrel@gmail.com>
2020-01-29 14:17:10 -0800

    added completely restructuring code after reading through a few articles on trees - realized my code was not working the way I thought it was working when building a tree

--- Notes/Algorithms/Data Structures/Trees.c
+++ Notes/Algorithms/Data Structures/Trees.c
@@ -25,34 +25,56 @@ void traverse(Node * root) {
         printf("%d \n", root->value);
         traverse(root->right);
     }
+    return ;
 }
 
-//To-do Note: may want to update addNode to take in a pointer to a Node for the later usage in other functions + to maintain ERROR/SUCCESS MESSAGE
-//addNode:
+
+//  addNode:
 //  Takes in a data value then creates a new node. If mem allocation does not fail then the node is assigned a value and left/right is set to NULL
-//  returns 0 if successful, -1 if an error occurs and -2 if memory allocation failure occurs. 
-Node * addNode(int data) {
-    Node* node = (Node*)malloc(sizeof(Node)); 
-    if (node != NULL) {
-        node->value = data;
-        node->left = NULL; 
-        node->right = NULL; 
-        return node;
-    } else {
-        printf("Memory allocation has failed.");
+//  Checks to see if the root Node provided is the root parent of the tree - if so sets value to data provided then left/right to NULL
+//  If it is a leaf to add to the tree checks to see which branch the value belongs to remain sorted
+//  returns root Node 
+Node * addNode(Node * root, int data) {
+    if (root == NULL) {
+        root = (Node*)malloc(sizeof(Node)); 
+        if (root != NULL) {
+            root->value = data;
+            root->left = NULL; 
+            root->right = NULL;
+        } 
+    } else if (data < root->value) {
+        root->left = addNode(root->left, data);
+    } else if (data > root->value) {
+        root->right = addNode(root->right, data);
     }
+    return root;
 }
 
+// finds the smallestNode and returns the node
 Node * smallestNode(Node * root) {
-    Node * current = root;
-    while (current && current->left != NULL) {
-        current = current->left;
-    }
-    return current;
+    if (root == NULL) {
+        return root;
+    } else if(root->left == NULL) {
+        return root;
+    } else {
+        return smallestNode(root->left);
+    }  
 }
-//Three possibilities: Node is a leaf and easily removed from tree
-//Node has only one child meaning that you just need to copy the child to the node then delete the child
-// Node to be deleted has too children
+
+// finds the largestNode and returns the node
+Node * largestNode(Node * root) {
+    if (root == NULL) {
+        return root;
+    } else if(root->right == NULL) {
+        return root;
+    } else {
+        return largestNode(root->left);
+    }  
+}
+
+// deleteNode takes in a root node and a value to delete
+// if/else figures out which branch the node would be based on value
+// returns root of updated tree
 Node * deleteNode(Node * root, int value) {
     if (root == NULL) {
         printf("Root node is empty.");
@@ -63,32 +85,18 @@ Node * deleteNode(Node * root, int value) {
         root->left = deleteNode(root->left, value);
     } else if (value > root->value) {
         root->right = deleteNode(root->right, value);
-    } else {
-        if (root->left == NULL) {
-            Node * temp = root->right;
-            free(root);
-            return temp;
-        } else if (root->right == NULL) {
-            Node * temp = root->left;
-            free(root);
-            return temp;
-        }
+    } else if (root->left && root->right){
         Node * temp = smallestNode(root->right);
         root->value = temp->value;
-        root->right = deleteNode(root->right, temp-> value);
-    }
-    return root;
-}
-
-// buildTree takes in an array of numbers, a root Node, integer and size of an array
-// Builds a tree in-line order with the provided array of numbers
-// Moved away from recursively implementing this function so as to allow for error messages 
-Node * buildTree(int *arr, Node * root, int i, int n) {
-    if (i < n) {
-        Node * temp = addNode(arr[i]);
-        root = temp;
-        root->left = buildTree(arr, root->left, 2 * i + 1, n);
-        root->right = buildTree(arr, root->right, 2 * i + 2, n);
+        root->right = deleteNode(root->right, root->value);
+    } else {
+        Node * temp = root;
+        if (root->left == NULL) {
+            root = root->right;
+        } else if (root->right == NULL) {
+            root = root->left;
+        }
+        free(temp);
     }
     return root;
 }
@@ -98,16 +106,16 @@ Node * buildTree(int *arr, Node * root, int i, int n) {
 Node * newTree() {
     int arr[9] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 }; 
     int n = sizeof(arr)/sizeof(arr[0]); 
-    Node * root = (Node*)malloc(sizeof(Node)); 
-    if (root != NULL) {
-        root = buildTree(arr, root, 0, n);
-        return root;
-    } else {
-        printf("An error has occurred.");
+    Node * root;
+    root = NULL;
+    for (int i = 0; i < n; i++) {
+        root = addNode(root, arr[i]);
     }
+    return root;
 }
 
 // reverseTree takes in a root node  of a tree then recursively reverses the tree
+// returns the root node
 Node * reverseTree(Node * root) {
     if (root == NULL) {
         return NULL;
@@ -117,17 +125,20 @@ Node * reverseTree(Node * root) {
     root->right = reverseTree(temp);
     return root;
 }
+
 //functions:
 //Delete Node
 //Reverse Tree
+//Search tree
 
 //main provides buildTree an array to insert in level order into a tree
 int main() {
-    
-   Node * root = newTree();
-   traverse(root);
-
-   reverseTree(root);
-   traverse(root);
-    
+    Node * root = newTree();
+    traverse(root);
+    deleteNode(root, 2);
+    printf("Deleting \n");
+    traverse(root);
+    reverseTree(root);
+    printf("Reversing \n");
+    traverse(root);
 }

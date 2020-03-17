0cb727ebc170956f0ab9ef9b57a0e5f03f3350f5
Katie Carrel <katiecarrel@gmail.com>
2020-03-17 13:27:39 -0700

    exercises

--- Notes/The Linux Programming Interface/Exercises/child_status/child_status.c
+++ Notes/The Linux Programming Interface/Exercises/child_status/child_status.c
@@ -1,6 +1,21 @@
 #include <sys/wait.h>
+#include <signal.h>
 #include "print_wait_status.h"          /* Declares printWaitStatus() */
 #include "tlpi_hdr.h"
+
+void printStatus(int code, int status) {
+    if (code == CLD_EXITED) {
+        printf("Child has exited with a status: %d\n", status);
+    } else if (code == CLD_KILLED) {
+        printf("Child was killed by the signal: %d (%s)\n", status, strsignal(status));
+    } else if (code == CLD_STOPPED) {
+        printf("Child was stopped by the signal: %d (%s)\n", status, strsignal(status));
+    } else if (code == CLD_CONTINUED) {
+        printf("Child continued. \n");
+    } else {
+        printf("Unexpected: Child has a status: (status=%x)\n", (unsigned int)status);
+    }
+}
 
 int main(int argc, char *argv[]) {
     int status;
@@ -25,23 +40,13 @@ int main(int argc, char *argv[]) {
     default:            /* Parent: repeatedly wait on child until it
                            either exits or is terminated by a signal */
         for (;;) {
-            childPid = waitpid(-1, &status, WUNTRACED
-#ifdef WCONTINUED       /* Not present on older versions of Linux */
-                                                | WCONTINUED
-#endif
-                    );
+            childPid = waitid(P_ALL, 0, &child, WEXITED | WSTOPPED | WCONTINUED);
             if (childPid == -1)
-                printf("waitpid");
-
-            /* Print status in hex, and as separate decimal bytes */
-
-            printf("waitpid() returned: PID=%ld; status=0x%04x (%d,%d)\n",
-                    (long) childPid,
-                    (unsigned int) status, status >> 8, status & 0xff);
-            printWaitStatus(NULL, status);
-
-            if (WIFEXITED(status) || WIFSIGNALED(status))
-                exit(EXIT_SUCCESS);
+                printf("waitid");
+            printf("waitid() returned: PID=%ld; status=0x%04x (%d,%d)\n",
+                    (long) child.si_pid, child.si_status,
+                    (unsigned int) child.si_status, child.si_status >> 8);
+            printStatus(NULL, status);
         }
     }
 }
